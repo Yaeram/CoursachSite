4ebe644b41cebb2efdf098ad5be3fa67b244c143
Maria Kozlova <116290571+Yaeram@users.noreply.github.com>
2022-12-28 13:28:27 +0300

    new

---
+++ JavaScript/Галерея.js
@@ -0,0 +1,33 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+}
+
+const buttons = document.querySelectorAll("[data-slider-button]");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.sliderButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-slider]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  })
+})
